a2070088df88914b413b2ea0890672b796e5322b
shiv kumar <kumarshiv_sk@outlook.com>
2025-03-21 19:23:12 -0400

    Added functionality to delete a record

--- Week12bCRUDCodeFirst/Controllers/HomeController.cs
+++ Week12bCRUDCodeFirst/Controllers/HomeController.cs
@@ -53,6 +53,33 @@ namespace Week12bCRUDCodeFirst.Controllers
             return View(Emp);
         }
 
+        //[HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var Emp = _db.Employees.FirstOrDefault(x => x.id == id);     // matching with database id
+            _db.Employees.Remove(Emp);
+            _db.SaveChanges();
+            return RedirectToAction("GetEmployeeData");
+        }
+
+
+        //[HttpGet]
+        //public IActionResult Update(int id)     //Getting edit-able page with id
+        //{
+        //    var Emp = _db.Employees.FirstOrDefault(x => x.id == id);     // matching id with database id
+
+        //    return View(Emp);
+        //}
+
+        //[HttpPost]
+        //public IActionResult Update(Employee obj)       // Saving edited page with information
+        //{
+        //    _db.Employees.Update(obj);
+        //    _db.SaveChanges();
+        //    //return View();
+        //    return RedirectToAction("GetEmployeeData");
+        //}
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
