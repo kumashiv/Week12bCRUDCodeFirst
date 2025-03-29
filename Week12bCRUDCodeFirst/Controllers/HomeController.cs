1ff63d5d465dc7784b84f9756491fc7f5b3a6906
shiv kumar <kumarshiv_sk@outlook.com>
2025-03-29 14:03:33 -0400

    - Default ILogger Constructor combined for Datacontext i.e. _db - Exception handling implemented on Create page for null values - Redirecting to default Error page for catch/ exception

--- Week12bCRUDCodeFirst/Controllers/HomeController.cs
+++ Week12bCRUDCodeFirst/Controllers/HomeController.cs
@@ -7,20 +7,21 @@ namespace Week12bCRUDCodeFirst.Controllers
 {
     public class HomeController : Controller
     {
-        //private readonly ILogger<HomeController> _logger;
-
-        //public HomeController(ILogger<HomeController> logger)
-        //{
-        //    _logger = logger;
-        //}
-
-
+        private readonly ILogger<HomeController> _logger;
         private readonly DataContext _db;
-
-        public HomeController(DataContext db)
+        public HomeController(ILogger<HomeController> logger,DataContext db)
         {
+            _logger = logger;
             _db = db;
         }
+
+
+        //private readonly DataContext _db;
+
+        //public HomeController(DataContext db)
+        //{
+        //    _db = db;
+        //}
 
         public IActionResult Index()
         {
@@ -40,9 +41,17 @@ namespace Week12bCRUDCodeFirst.Controllers
         [HttpPost]
         public IActionResult Create(Employee obj)
         {
-            _db.Employees.Add(obj);     // adding to SQL query    //constructor part   
-                                        // Employees is the table name defined in DataContext.cs
-            _db.SaveChanges();      // Saving
+            try
+            {
+                _db.Employees.Add(obj);     // adding to SQL query    //constructor part   
+                                            // Employees is the table name defined in DataContext.cs
+                _db.SaveChanges();      // Saving
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine(ex.Message);
+                return RedirectToAction("Error");
+            }
             return View();
         }
 
@@ -81,7 +90,7 @@ namespace Week12bCRUDCodeFirst.Controllers
         }
 
 
-
+        // Error
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
